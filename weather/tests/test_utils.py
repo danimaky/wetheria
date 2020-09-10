694b5a6a6bfbc4d7b9584b9e35146c6e58b75e50
Daniel Nuñez <daniel.nunez.ext@nokia.com>
2020-09-09 19:27:09 -0500

    Created function to clean data and output as desired

--- weather/tests/test_utils.py
+++ weather/tests/test_utils.py
@@ -1,59 +1,60 @@
 from unittest.mock import patch
-from weather.utils import fetch_information_from_weather_api
+from weather.utils import fetch_information_from_weather_api, clean_weather_api_response
 from requests.exceptions import RequestException
+from freezegun import freeze_time
 
 
 @patch("weather.utils.requests.get")
-def test_fetch_information_from_weather_api_well_response(mock_get):
-    INFORMATION = {
+def test_fetch_information_from_weather_api_ok_response(mock_get):
+    information = {
         "coord": {
-            "lon":-74.08,
-            "lat":4.61
+            "lon": -74.08,
+            "lat": 4.61
         },
-        "weather":[
+        "weather": [
             {
-                "id":802,
-                "main":"Clouds",
-                "description":"scattered clouds",
-                "icon":"03n"
+                "id": 802,
+                "main": "Clouds",
+                "description": "scattered clouds",
+                "icon": "03n"
             }
         ],
-        "base":"stations",
-        "main":{
-            "temp":283.15,
-            "feels_like":282.21,
-            "temp_min":283.15,
-            "temp_max":283.15,
-            "pressure":1027,
-            "humidity":93
+        "base": "stations",
+        "main": {
+            "temp": 283.15,
+            "feels_like": 282.21,
+            "temp_min": 283.15,
+            "temp_max": 283.15,
+            "pressure": 1027,
+            "humidity": 93
         },
-        "visibility":10000,
-        "wind":{
-            "speed":1,
-            "deg":0
+        "visibility": 10000,
+        "wind": {
+            "speed": 1,
+            "deg": 0
         },
-        "clouds":{
-            "all":40
+        "clouds": {
+            "all": 40
         },
-        "dt":1599619177,
-        "sys":{
-            "type":1,
-            "id":8582,
-            "country":"CO",
-            "sunrise":1599562134,
-            "sunset":1599605947
+        "dt": 1599619177,
+        "sys": {
+            "type": 1,
+            "id": 8582,
+            "country": "CO",
+            "sunrise": 1599562134,
+            "sunset": 1599605947
         },
-        "timezone":-18000,
-        "id":3688689,
-        "name":"Bogotá",
-        "cod":200
+        "timezone": -18000,
+        "id": 3688689,
+        "name": "Bogotá",
+        "cod": 200
     }
     mock_get.return_value.status_code = 200
-    mock_get.return_value.json = lambda : INFORMATION
+    mock_get.return_value.json = lambda: information
     response = fetch_information_from_weather_api("Bogota", "co")
     assert mock_get.called
     assert mock_get.call_count == 1
-    assert response == INFORMATION
+    assert response == information
 
 
 @patch("weather.utils.requests.get")
@@ -70,3 +71,79 @@ def test_fetch_information_from_weather_api_connection_error_retry(mock_sleep, m
     assert mock_sleep.call_count == 2
     assert mock_get.called
     assert mock_get.call_count == 3
+
+
+@freeze_time("2020-09-09 03:57:00", -5)
+@patch("weather.utils.requests.get")
+def test_clean_weather_api_response_ok(mock_get):
+    information = {
+        "coord": {
+            "lon": -74.08,
+            "lat": 4.61
+        },
+        "weather": [
+            {
+                "id": 802,
+                "main": "Clouds",
+                "description": "scattered clouds",
+                "icon": "03n"
+            }
+        ],
+        "base": "stations",
+        "main": {
+            "temp": 283.15,
+            "feels_like": 282.21,
+            "temp_min": 283.15,
+            "temp_max": 283.15,
+            "pressure": 1027,
+            "humidity": 93
+        },
+        "visibility": 10000,
+        "wind": {
+            "speed": 1,
+            "deg": 0
+        },
+        "clouds": {
+            "all": 40
+        },
+        "dt": 1599619177,
+        "sys": {
+            "type": 1,
+            "id": 8582,
+            "country": "CO",
+            "sunrise": 1599562134,
+            "sunset": 1599605947
+        },
+        "timezone": -18000,
+        "id": 3688689,
+        "name": "Bogotá",
+        "cod": 200
+    }
+    expected_output = {
+        "location_name": "Bogotá, CO",
+        "temperature": "10 °C",
+        "wind": {'deg': 0, 'speed': 1},
+        "cloudines": "Scattered clouds",
+        "presure": "1027 hpa",
+        "humidity": "93%",
+        "sunrise": "05:48",
+        "sunset": "17:59",
+        "geo_coordinates": "[4.61, -74.08]",
+        "requested_time": "2020-09-08 22:57:00"
+    }
+    mock_get.return_value.status_code = 200
+    mock_get.return_value.json = lambda: information
+    response = fetch_information_from_weather_api("Bogota", "co")
+    cleaned_data = clean_weather_api_response(response)
+    assert cleaned_data == expected_output
+
+
+@patch("weather.utils.requests.get")
+def test_clean_weather_api_response_not_found(mock_get):
+    information = {"cod": "404", "message": "city not found"}
+    mock_get.return_value.status_code = 404
+    mock_get.return_value.json = lambda: information
+    response = fetch_information_from_weather_api("XXX", "ll")
+    expected_output = {"message": "city not found"}
+    cleaned_data = clean_weather_api_response(response)
+    assert cleaned_data == expected_output
--- weather/utils.py
+++ weather/utils.py
@@ -1,9 +1,10 @@
 from wetheria.settings import WEATHER_API_URL, WEATHER_API_TOKEN
 import time
 import requests
+import datetime
 
 
-def fetch_information_from_weather_api(city, country_code, retry=1):
+def fetch_information_from_weather_api(city: str, country_code: str, retry: int = 1):
     params = {
         "q": f"{city},{country_code}",
         "appid": WEATHER_API_TOKEN
@@ -17,3 +18,25 @@ def fetch_information_from_weather_api(city, country_code, retry=1):
         else:
             raise e
     return response.json()
+
+
+def clean_weather_api_response(raw_data: dict = dict()):
+    cleaned_data = {}
+    if raw_data['cod'] == 200:
+        temperature = raw_data['main']['temp']
+        temperature = int(temperature - 273.15)
+        cleaned_data = {
+            "location_name": f"{raw_data.get('name', 'N/A')}, {raw_data.get('sys', {}).get('country')}",
+            "temperature": f"{temperature} °C",
+            "wind": raw_data["wind"],
+            "cloudines": raw_data["weather"][0]["description"].capitalize(),
+            "presure": f"{raw_data['main']['pressure']} hpa",
+            "humidity": f"{raw_data['main']['humidity']}%",
+            "sunrise": f"{datetime.datetime.fromtimestamp(raw_data['sys']['sunrise']).strftime('%H:%M')}",
+            "sunset": f"{datetime.datetime.fromtimestamp(raw_data['sys']['sunset']).strftime('%H:%M')}",
+            "geo_coordinates": f"[{raw_data['coord']['lat']}, {raw_data['coord']['lon']}]",
+            "requested_time": f"{datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')}",
+        }
+    else:
+        cleaned_data['message'] = raw_data['message']
+    return cleaned_data
